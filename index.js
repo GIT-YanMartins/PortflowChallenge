6ce988139b9702422ba2fe0d6785c2f7368b7372
GIT-YanMartins <yangabriel15@outlook.com>
2023-01-11 15:20:13 -0300

    Adicionadas constantes novas

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const nome = prompt('Qual o seu nome?');
-alert ('Que bom ter você aqui ${come}! \n Vamos começar?')
+const sobrenome = prompt ('Qual o seu sobrenome?')
+alert (`Que bom ter você aqui ${nome} ${sobrenome}! \n Vamos começar?`);
